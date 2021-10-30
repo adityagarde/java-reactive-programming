646034370f12d3d7b2501ba703a9982e60a61977
adityagarde <aditya.garde96@gmail.com>
2021-10-30 15:10:40 +0530

    Mono with block()

--- src/main/java/com/github/adityagarde/reactor/sec01/_06SupplierPipeline.java
+++ src/main/java/com/github/adityagarde/reactor/sec01/_06SupplierPipeline.java
@@ -16,11 +16,17 @@ public class _06SupplierPipeline {
         getName().subscribe(item -> System.out.println(item));
         getName();
 
+        // Async
         getName().subscribeOn(Schedulers.boundedElastic())
                 .subscribe(item -> System.out.println(item));
         getName();
 
         Util.sleepSeconds(4); // Added just to see the output
+
+        // Adding block only for testing purpose, blocking call, Sleep not required.
+        String str = getName().subscribeOn(Schedulers.boundedElastic())
+                .block();
+        System.out.println(str);
 
         /** OUTPUT of the above code!
          * Inside getName()
@@ -28,12 +34,15 @@ public class _06SupplierPipeline {
          * Inside getName()
          * Inside getName()
          * Generating Name...
-         * SQUIRTLE
+         * NIDOKING
          * Inside getName()
          * Inside getName()
          * Inside getName()
          * Generating Name...
-         * SANDSLASH
+         * CHARMANDER
+         * Inside getName()
+         * Generating Name...
+         * ELECTRODE
          */
     }
 
